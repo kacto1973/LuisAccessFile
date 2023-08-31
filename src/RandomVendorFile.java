5af2497b787faab2ba21226c1cd1d2b59f20ce4b
Luis Morales <luisalbertomm18@hotmail.com>
2023-08-31 10:21:24 -0700

    Initial commit

---
+++ src/RandomVendorFile.java
@@ -0,0 +1,140 @@
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.util.Date;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class RandomVendorFile {
+    private String fileName;
+
+    public RandomVendorFile(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public long write(Vendor v) {
+        RandomAccessFile out = null;
+        long position = 0;
+        byte buffer[] = null;
+
+        try {
+            out = new RandomAccessFile(fileName, "rws");
+
+            // cuantos bytes hay en archivo
+            position = out.length();
+
+            // ir al ultimo byte
+            out.seek(position);
+
+            // escribir el codigo
+            out.writeInt(v.getCodigo());
+
+            // escribir los bytes que se requieren para imprimir
+            // la cadena con el nombre
+            buffer = v.getNombre().getBytes();
+            out.write(buffer);
+
+            // convertir de Date a long
+            long dob = v.getFecha().getTime();
+            out.writeLong(dob);
+
+            // escribir los bytes que se requieren para imprimir
+            // la cadena con la zona
+            buffer = v.getZona().getBytes();
+            out.write(buffer);
+
+            //escribir el Long de ventas
+            out.writeLong(v.getVentas());
+            out.close();
+        } catch (IOException ex) {
+            Logger.getLogger(RandomVendorFile.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return position;
+    }
+
+
+    public Vendor read(long position) {
+        RandomAccessFile out = null;
+        long bytes = 0;
+        byte buffer[] = null;
+        Vendor vendor = null;
+        try {
+            out = new RandomAccessFile(fileName, "rws");
+            out.seek(position);
+
+            int codigo = out.readInt();
+
+            byte[] nameBytes = new byte[Vendor.MAX_NAME];
+            out.read(nameBytes);
+
+            long dateBytes = out.readLong();
+
+            byte[] zonaBytes = new byte[Vendor.MAX_ZONE];
+            out.read(zonaBytes);
+            Long ventas=out.readLong();
+
+            vendor = new Vendor(codigo, new String(nameBytes), new Date(dateBytes),
+                    new String(zonaBytes),ventas);
+            out.close();
+
+        } catch (IOException ex) {
+            Logger.getLogger(RandomVendorFile.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return vendor;
+    }
+
+    public void read(Vendor vendors[]) {
+        RandomAccessFile out = null;
+        long bytes = 0;
+        byte buffer[] = null;
+        Vendor vendor = null;
+        try {
+            out = new RandomAccessFile(fileName, "rws");
+            for (int i = 0; i < vendors.length; i++) {
+
+                int codigo = out.readInt();
+
+                byte[] nameBytes = new byte[Vendor.MAX_NAME];
+                out.read(nameBytes);
+
+                long dateBytes = out.readLong();
+
+                byte[] zonaBytes = new byte[Vendor.MAX_ZONE];
+                out.read(zonaBytes);
+                Long ventas=out.readLong();
+
+                vendors[i] = new Vendor(codigo, new String(nameBytes), new Date(dateBytes),
+                        new String(zonaBytes),ventas);
+            }
+            out.close();
+
+        } catch (IOException ex) {
+            Logger.getLogger(RandomVendorFile.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+
+    public static void main(String[] args) {
+
+        final String dataPath = "vendors-data.dat";
+
+        RandomVendorFile randomFile = new RandomVendorFile(dataPath);
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Numero de registro:");
+
+        int n = input.nextInt();
+
+        int pos = (n * Vendor.RECORD_LEN) - Vendor.RECORD_LEN;
+
+        long t1 = System.currentTimeMillis();
+        Vendor p = randomFile.read(pos);
+        long t2 = System.currentTimeMillis();
+        System.out.println(p);
+        System.out.println(t2 - t1);
+
+    }
+
+}
---
+++ src/Vendor.java
@@ -0,0 +1,129 @@
+import java.io.StringReader;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.Scanner;
+
+public class Vendor {
+    public static final int MAX_NAME = 35;
+    public static final int MAX_ZONE = 15;
+
+    public static final int RECORD_LEN = 62;
+
+    private int codigo;      // 4 bytes
+    private String nombre;   // 35 bytes
+    private Date fecha;      // 8 bytes, almacenado en long
+    private String zona;     // 15 bytes
+    private Long ventas;
+
+    public Vendor(int codigo, String nombre, Date fecha, String zona, Long ventas) {
+        super();
+
+        this.setCodigo(codigo);
+        this.setNombre(nombre);
+        this.setFecha(fecha);
+        this.setZona(zona)  ;
+        this.setVentas(ventas);
+    }
+
+    public Vendor(int codigo, String nombre, String fecha, String zona,Long ventas) {
+        super();
+        this.setCodigo(codigo);
+        this.setNombre(nombre);
+        this.setFecha(fecha);
+        this.setZona(zona)  ;
+        this.setVentas(ventas);
+    }
+
+    public Vendor() {
+        super();
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public String getZona() {
+        return zona;
+    }
+
+    public Long getVentas() {
+        return ventas;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+
+    public void setNombre(String nombre) {
+        int len = nombre.length();
+        if( len < MAX_NAME ) {
+            StringBuilder sb = new StringBuilder(nombre);
+            int count = MAX_NAME - len;
+            // rellenar con espacios en blanco a la derecha
+            for(int i = 0;  i<count; i++) {
+                sb.append(' ');
+            }
+            this.nombre = sb.toString();
+        } else {
+            this.nombre = nombre;
+        }
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public void setVentas(Long ventas) {
+        this.ventas = ventas;
+    }
+
+    public void setFecha(String fecha) {
+        //StringTokenizer parser = new StringTokenizer(fecha,"/");
+
+        Scanner parser = new Scanner( new StringReader(fecha) );
+        parser.useDelimiter("/");
+
+        int month = parser.nextInt();
+        int day = parser.nextInt();
+        int year = parser.nextInt();
+
+        GregorianCalendar calendar = new GregorianCalendar(year,month, day);
+
+        this.fecha = calendar.getTime();
+    }
+
+    public void setZona(String zona) {
+        int len = zona.length();
+        if( len < MAX_ZONE ) {
+            StringBuilder sb = new StringBuilder(zona);
+            int count = MAX_ZONE - len;
+            for(int i = 0;  i < count; i++) {
+                sb.append(' ');
+            }
+            this.zona = sb.toString();
+        } else {
+            this.zona = zona;
+        }
+    }
+
+    public String fechaString() {
+        SimpleDateFormat f = new SimpleDateFormat("dd/MM/yyyy");
+        return f.format(fecha);
+    }
+    @Override
+    public String toString() {
+        SimpleDateFormat f = new SimpleDateFormat("dd/MM/yyyy");
+        // regresa una cadena en formato CSV
+        return codigo + "," + nombre + "," + f.format(fecha) + "," + zona;
+    }
+}
