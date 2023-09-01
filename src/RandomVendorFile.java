200109ab7787bc4b2326e772aa6fa5bbc07620f6
Luis <a222212359@unison.mx>
2023-08-31 20:21:41 -0700

    prueba

--- src/RandomVendorFile.java
+++ src/RandomVendorFile.java
@@ -6,6 +6,8 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class RandomVendorFile {
+
+    //testeoaaaaaaaaaaa
     private String fileName;
 
     public RandomVendorFile(String fileName) {
